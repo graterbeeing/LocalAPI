def3979c6535a417fca2318bb1304faf1bb6c333
graterbeeing <oz.braverman@gmail.com>
2024-12-05 14:01:07 +0200

    Add project files.

---
+++ UseBD/Program.cs
@@ -0,0 +1,46 @@
+using UseBD.Components;
+using UseBD.Services;
+
+namespace UseBD
+{
+    public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+			builder.Services.AddRazorComponents()
+				.AddInteractiveServerComponents();
+
+            builder.Services.AddServerSideBlazor();
+
+            builder.Services.AddSingleton<User_service>();
+
+            builder.Services.AddHttpClient("APIClient", client =>
+            {
+                client.BaseAddress = new Uri("http://localhost:5210"); // עדכן לכתובת הנכונה של ה-API שלך
+            });
+
+            var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseStaticFiles();
+			app.UseAntiforgery();
+
+			app.MapRazorComponents<App>()
+				.AddInteractiveServerRenderMode();
+
+			app.Run();
+		}
+	}
+}
---
+++ UseBD/Services/User_service.cs
@@ -0,0 +1,17 @@
+﻿namespace UseBD.Services
+{
+    public class User_service
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public int Role { get; set; }
+
+        // פונקציה לאיפוס הנתונים במקרה של יציאה מהמערכת
+        public void ClearUser()
+        {
+            UserId = 0;
+            UserName = string.Empty;
+            Role = 0;
+        }
+    }
+}
