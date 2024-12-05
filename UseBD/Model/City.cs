8b0b7e1e848f9a12444b132b359109051eab1847
graterbeeing <oz.braverman@gmail.com>
2024-12-05 14:01:07 +0200

    Add project files.

---
+++ UseBD/Model/City.cs
@@ -0,0 +1,10 @@
+﻿namespace UseBD.Model
+{
+	public class City
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Population { get; set; }
+	}
+
+}
---
+++ UseBD/Model/investment.cs
@@ -0,0 +1,13 @@
+﻿namespace UseBD.Model
+{
+    public class Investment
+    {
+        public int inv_Id { get; set; }
+        public int user_id { get; set; }
+        public int type_id { get; set; }
+        public int option_id { get; set; }
+        public decimal amount_invested { get; set; }
+        public DateTime investment_date { get; set; }
+    }
+
+}
